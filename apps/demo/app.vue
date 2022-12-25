1c7a2e1e07e66ba6ca80f605fa83cd69b88fdcbe
zengchaowu <root@zengchaowu.com>
2022-12-25 15:25:39 +0800

    chore: init button

--- apps/demo/app.vue
+++ apps/demo/app.vue
@@ -1,5 +1,14 @@
+<script setup>
+import DoraButton from '@dora-ui/button'
+
+const state = reactive({ count: 0 })
+const buttons = reactive([{ label: 'S', labelClass: ['text-red', 'text-2xl'] }, {label: 'S'}, {label: 'S'}])
+
+</script>
+
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col gap-5">
+    <DoraButton v-for="button in buttons" :payload="button" />
   </div>
 </template>
---
+++ components/button/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps(['payload'])
+</script>
+
+<template>
+    <button type="button" :class="payload?.buttonClass">
+        <span :class="payload?.labelClass">{{ payload?.label }}</span>
+    </button>
+</template>
