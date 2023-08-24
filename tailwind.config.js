15afb01a792f6dc1f075da8ce8a7d5831799b980
zengcw <zengcw@300624.cn>
2023-08-24 19:47:00 +0800

    chore: custom tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const sizes = {
+  none: '0',
+  sm: '0.125rem',
+  DEFAULT: '0.25rem',
+  DEFAULT: '4px',
+  md: '0.375rem',
+  lg: '0.5rem',
+  full: '9999px',
+  large: '12px',
+};
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    spacing: sizes,
+    borderRadius: sizes,
+    colors: {
+      primary: '#9751ff',
+      text: '#000000',
+      textInverse: '#ffffff',
+    },
   },
   plugins: [],
-}
-
+};
