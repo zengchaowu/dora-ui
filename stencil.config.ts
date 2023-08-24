d9d6a28964d46f6a3b301f139c070c2044137b23
zengcw <zengcw@300624.cn>
2023-08-24 19:23:51 +0800

    chore: setup tailwind

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'dora-ui',
+  globalStyle: 'www/tailwind.css',
   outputTargets: [
     {
       type: 'dist',
